e85f2965ef4edf3489a01a749bfa2348d4250f5c
VitorGuedesMadeira <vgm_rox@hotmail.com>
2023-04-26 14:40:53 -0300

    Add NEW and CREATE game functionalities

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,27 @@
 class GamesController < ApplicationController
   def show; end
 
-  def new; end
+  def new
+    @game = Game.new
+  end
 
-  def create; end
+  def create
+    @game = Game.new(params_required)
+
+    if @game.save
+      redirect_to game_path(@game), notice: 'Game was successfully created!'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   def edit; end
 
   def update; end
+
+  private
+
+  def params_required
+    params.require(:game).permit(:state, :players, :turn, :moves, :time, :status, :theme, :mode)
+  end
 end
