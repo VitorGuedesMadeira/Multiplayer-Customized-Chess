fdff2fd8f1cf1d1dd82a65b7dca9a02f5f1ab363
Diego <diegoyon@outlook.com>
2023-05-16 11:44:13 -0600

    add timers

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-import "./components/close-button"
-import "./components/promotion-button"
+import '@hotwired/turbo-rails';
+import 'controllers';
+import './components/close-button';
+import './components/promotion-button';
+import './components/countdown';
